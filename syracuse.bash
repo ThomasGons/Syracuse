5cedb5d2c598eee146fd7289f27fb5e1d13f8106
thomas <gons.thomas@gmail.com>
2022-02-06 18:23:13 +0100

    final

--- syracuse.bash
+++ syracuse.bash
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-SYNTH="Synthesis/minMax.txt"
+sy=0
 
+while getopts "h(help)c(clean)m(manual)s(synthesis):" opt
+do
+    case $opt in
+        h)
+            cat 'Config/help' ; exit
+            ;;
+        m)
+            man './Config/syracuse.8.gz'; exit
+            ;;
+        c)
+            rm -rf Data Images Synthesis exe; echo "Project has been cleaned up !"; exit
+            ;;
+        s)
+            sy=1; shift
+            ;;
+    esac
+done
+
+SYNTH="Synthesis/$1_$2.txt"
 
 mkdir -p Images Data Synthesis
 rm -f Images/*
 
-gcc syracuse.c -O3 -o exe
+if [[ -e exe || syracuse.c -nt exe ]]; then gcc syracuse.c -O3 -o exe; fi
 
+
+if [ $# -ne 2 ]; then echo "This program required two terminals"; exit; fi
+if [ $(echo $1$2 | grep -Po "\D" | head -n 1) ]; then echo "The terminals should be positive integers"; exit; fi 
+
+echo -ne "Collecting data...\r"
 for i in $(seq $1 $2) 
 do 
     ./exe $i Data/$i.dat
-    if [ $? -ne 0 ]
-    then
-        exit
-    fi
+    if [ $? -ne 0 ]; then exit; fi
     head -n -3 Data/$i.dat | tail -n +2 >> Data/allData.dat; echo '' >> Data/allData.dat # all Un and n for each file
     args=(`tail -n -3 Data/$i.dat | cut -d ':' -f2`)
     echo $i ${args[0]} >> Data/altitudeMax.dat
@@ -22,19 +43,11 @@ do
     echo $i ${args[2]} >> Data/altitudeDuration.dat
 done
 
+echo -e "\033[0;32mData collected.   \033[0m"
+
 img=('allData' 'altitudeMax' 'flightDuration' 'altitudeDuration')
-rm $SYNTH
 
-for i in $(seq 1 3)
-do
-    echo "${img[i]}:" >> $SYNTH
-    NR=$(wc -l Data/${img[i]}.dat)
-    echo -ne "\tavg: " >> $SYNTH ; echo "scale=2; $(awk '{ sum += $2 } END { printf sum / NR}' Data/${img[i]}.dat)" | bc >> $SYNTH # average
-    UO=(`sort -g -k2 Data/${img[i]}.dat | sed -n '1p; $p' | cut -d' ' -f1`)
-    MINMAX=(`sort -g -k2 Data/${img[i]}.dat | sed -n '1p; $p' | cut -d' ' -f2`)
-    echo -e "\tmin: ${MINMAX[0]} (uo = ${UO[0]})" >> $SYNTH; echo -e "\tmax: ${MINMAX[1]} (uo = ${UO[1]})" >> $SYNTH
-done
-
+echo -ne "Create all graphs...\r"
 for i in $(seq 0 3)
 do
         gnuplot -persist <<-EOFMarker
@@ -47,10 +60,28 @@ do
         } else {
             set xlabel "uo"; set ylabel "${img[i]}" 
         }
-        plot "Data/${img[i]}.dat" using 1:2 with lines lt rgb '#005D70' lw 1 title "${img[i]}.dat"
+        plot "Data/${img[i]}.dat" using 1:2 with lines lt rgb "#005D70" lw 1 title "${img[i]}.dat"
 EOFMarker
 done
+echo -e "\033[0;32mGraphs Created.\033[0m       "
 
-echo -e '\e[1m\e[4mStats\n\e[0m' ; cat ${SYNTH}
+rm -f ${SYNTH}
+
+echo -ne "Generating synthesis...\r"
+
+for i in $(seq 1 3)
+do
+    echo "${img[i]}:" >> $SYNTH
+    NR=$(wc -l Data/${img[i]}.dat)
+    echo -ne "\tavg: " >> $SYNTH ; echo "scale=2; $(awk '{ sum += $2 } END { printf sum / NR}' Data/${img[i]}.dat)" | bc -l >> $SYNTH # average
+    UO=(`sort -g -k2 Data/${img[i]}.dat | sed -n '1p; $p' | cut -d' ' -f1`)
+    MINMAX=(`sort -g -k2 Data/${img[i]}.dat | sed -n '1p; $p' | cut -d' ' -f2`)
+    echo -e "\tmin: ${MINMAX[0]} (uo = ${UO[0]})" >> $SYNTH; echo -e "\tmax: ${MINMAX[1]} (uo = ${UO[1]})" >> $SYNTH
+done
+
+echo -e "\033[0;32mSynthesis generated.\033[0m   "
+
+if [ $sy -eq 1 ]; then echo -e '\e[1m\e[4mStats\n\e[0m' ; cat ${SYNTH}; fi
+
 rm -f Data/*
 
--- syracuse.c
+++ syracuse.c
@@ -1,52 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define SYRACUSE(ui) (ui) % 2 ? (ui) * 3 + 1: (ui) / 2
-#define MAX(a, b) (a) > (b) ? (a): (b)
-
-unsigned charToUnsigned(char* p);
-void syracuse(char **argv);
 
 int main(int argc, char **argv){
-    if (argc != 3){
-        fprintf(stderr, "Bad usage: ./exe Uo filename.\n");
-        exit(1);
-    }
-    if (!charToUnsigned(argv[1])){
-        fprintf(stderr, "Please enter a strictly positive integer.");
-        exit(EXIT_FAILURE);
-    }
-    syracuse(argv);
-    exit(EXIT_SUCCESS);
-}
-
-unsigned charToUnsigned(char* p){
-    unsigned n = 0;
-    for (unsigned char i = 0; i < strlen(p); i++){
-        if (p[i] >= 48 && p[i] <= 57)
-            n = n * 10 + (p[i] % 48);
-        else {
-            fprintf(stderr, "%s is not a number or a positive integer.\n", p);
-            exit(2);
-        }
-    }
-    return n;
-}
-
-void syracuse(char **argv){
     FILE* f = fopen(argv[2], "w");
     if (!f){
         fprintf(stderr, "File <%s> cannot be opened.\n", argv[2]);
         exit(3);
     }
     unsigned short i = 0, altDur = 0, inAlt = 1;
-    unsigned uo = charToUnsigned(argv[1]), ui = uo, altMax = uo;
+    unsigned uo = strtol(argv[1], NULL, 0), ui = uo, altMax = uo;
     fprintf(f, "n, Un\n%hhu %u\n", i, ui);
     do{
-        ui = SYRACUSE(ui);
+        ui = ui % 2 ? ui * 3 + 1: ui / 2;
         if (ui > uo){
-            altMax = MAX(altMax, ui);
+            altMax = alMax > ui ? altMax: ui;
             if (inAlt)
                 altDur++;
         }
@@ -57,4 +24,5 @@ void syracuse(char **argv){
     } while(ui != 1);
     fprintf(f, "AltitudeMax: %u\nFlightDuration: %hhu\nAltitudeDuration: %hhu\n", altMax, i, altDur);
     fclose(f);
+    exit(EXIT_SUCCESS);
 }
